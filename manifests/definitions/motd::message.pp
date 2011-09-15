5067f71c8a18381c28a000e6f632b3f5a6d5f969
Marc Fournier <marc.fournier@camptocamp.com>
2011-09-15 18:57:55 +0200

    motd: new module to manage login messages.

---
+++ manifests/definitions/motd::message.pp
@@ -0,0 +1,19 @@
+define motd::message ($source='', content='') {
+
+  include motd
+
+  common::concatfilepart { $name:
+    file   => "${motd::path}",
+    manage => true,
+    source => $source ? {
+      default => $source,
+      '' => false,
+    },
+    content => $content ? {
+      default => $content,
+      '' => false,
+    },
+    notify => Exec["update motd"],
+  }
+
+}
