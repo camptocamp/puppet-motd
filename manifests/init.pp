3348418ad632f41da9fa691cf89a0bb42a207523
Marc Fournier <marc.fournier@camptocamp.com>
2011-11-15 13:05:21 +0100

    file renaming to be compliant with recommended module structure.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,1 +1,16 @@
-import "*/*.pp"
+class motd {
+
+  $path = $operatingsystem ? {
+    /RedHat|CentOS|Fedora/ => "/etc/motd",
+    /Debian|Ubuntu/ => "/etc/motd.tail",
+  }
+
+  # debian rewrites his motd, see /etc/init.d/bootmisc.sh
+  exec { "update motd":
+    refreshonly => true,
+    command     => $operatingsystem ? {
+      /RedHat|CentOS|Fedora/ => "true",
+      /Debian|Ubuntu/ => "uname -snrvm > /var/run/motd && cat /etc/motd.tail >> /var/run/motd",
+    },
+  }
+}
