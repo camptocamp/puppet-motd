e8e5a0e5f362dec41ea0baa33313c2eb576ef323
Andreas Jaggi <andreas.jaggi@waterwave.ch>
2013-05-18 10:36:55 +0200

    Add test cases for motd::message

---
+++ spec/defines/message_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+
+describe 'motd::message', :type => :define do
+  let :title do
+    'somename'
+  end
+
+  it { should include_class('motd') }
+
+  context 'population via source file' do
+    let :params do
+      {
+        :source  => '/somefile',
+        :content => '',
+      }
+    end
+    it { should contain_concat_fragment('somename').with(
+      source  => '/somefile',
+      content => undef
+    ) }
+  end
+
+  context 'population via content param' do
+    let :params do
+      {
+        :source  => '',
+        :content => 'somecontent',
+      }
+    end
+    it { should contain_concat_fragment('somename').with(
+      source  => undef,
+      content => 'somecontent'
+    ) }
+  end
+end
